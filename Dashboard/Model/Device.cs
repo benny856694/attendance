7157f6fa2908dbd6869c7b371681730d6048f81e
benny <shenbinsc@qq.com>
2021-06-09 01:06:11 +0800

    cosmetic

--- Dashboard/Model/Device.cs
+++ Dashboard/Model/Device.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Dashboard.Model
 {
-    public class Device
+    public class Device : IEquatable<Device>
     {
         public string Name { get; set; }
         public string IP { get; set; }
@@ -18,6 +18,20 @@ namespace Dashboard.Model
         {
             return Name == null ? IP : $"{Name} | {IP}";
         }
-        
+
+        public override bool Equals(object obj) => this.Equals(obj as Device);
+       
+
+        public bool Equals(Device other)
+        {
+            if (other is null) return false;
+
+            if (object.ReferenceEquals(this, other)) return true;
+
+            if (this.GetType() != other.GetType()) return false;
+
+
+            return this.IP == other.IP;
+        }
     }
 }
