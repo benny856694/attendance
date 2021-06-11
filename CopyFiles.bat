850e34cc6bcde475478c26de7648c6e9f3cd4d95
benny <shenbinsc@qq.com>
2021-06-11 23:28:11 +0800

    add deepleeds logo and update copy file scripts

--- CopyFiles.bat
+++ CopyFiles.bat
@@ -1,4 +1,39 @@
 ::将编译后的文件移动到Setup\InstallFiles文件夹
+@ECHO off
+cls
+:menu
+ECHO.
+ECHO 请选择品牌
+ECHO 0. 缺省(Tommi)
+ECHO 1. Mox
+ECHO 2. SmartSchoolNetwork
+ECHO 3. Deepleeds
+set choice=
+set /p choice=请输入数字选择品牌.
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='0' goto default
+if '%choice%'=='1' goto mox
+if '%choice%'=='2' goto SmartSchoolNetwork
+if '%choice%'=='3' goto Deepleeds
+ECHO "%choice%" 无效, 请重新输入
+ECHO.
+goto menu
+:default
+set brand=default
+goto start
+:mox
+set brand=mox
+goto start
+:SmartSchoolNetwork
+set brand=smartschoolnetwork
+goto start
+:Deepleeds
+set brand=deepleeds
+goto start
+
+:start
+ECHO 你选择了 %brand%
+pause
 
 ::删除文件
 rmdir /s/q Setup\InstallFiles
@@ -16,4 +51,4 @@ xcopy WinfromUI\bin\Release Setup\InstallFiles /e /y /exclude:copy_exclude.txt
 xcopy ..\HaSdkDemoCsharp\lib\ Setup\InstallFiles /e /y /exclude:copy_exclude.txt
 xcopy ..\AttendanceWeb\dist Setup\InstallFiles\detached /e /y /exclude:copy_exclude.txt
 xcopy tool Setup\InstallFiles /e /y
-xcopy .\brandings Setup\InstallFiles\branding /e /y
+xcopy .\brandings\%brand%\ Setup\InstallFiles\branding /e /y
