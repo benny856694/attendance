81111da2d15c5531d42547df51ee2efc617d4167
benny <shenbinsc@qq.com>
2021-06-11 23:49:59 +0800

    cosmetic

--- Make.bat
+++ Make.bat
@@ -1,8 +1,8 @@
-call ../build.bat
+//call ../build.bat
 devenv huaanClient.sln /clean "release|x86"
 devenv huaanClient.sln /rebuild "release|x86"
 devenv ../HaSdkDemoCsharp/SDKClientSharp.sln /clean "release|x86"
 devenv ../HaSdkDemoCsharp/SDKClientSharp.sln /rebuild "release|x86"
-call buildToSetup
+//call buildToSetup
 //cd Setup
 //"C:\Program Files (x86)\Inno Setup 6\iscc.exe" package.iss
