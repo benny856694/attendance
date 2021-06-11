af155229d84ebd650215437b344afed22779fc45
benny <shenbinsc@qq.com>
2021-06-11 13:57:47 +0800

    time display issue

--- Dashboard/Form1.cs
+++ Dashboard/Form1.cs
@@ -240,7 +240,7 @@ namespace Dashboard
 
                     var bgc = e.IsPersonMatched ? Color.Green : Color.Red;
                     var name = e.PersonName ?? "Unidentified";
-                    control.BottomText = $"{name} | {e.CaptureTime:T}";
+                    control.BottomText = $"{name} | {e.CaptureTime.ToLocalTime():T}";
                     control.BackColor = bgc;
                 }));
             }
