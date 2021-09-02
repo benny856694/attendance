e4c0a437e054a67412ba44e7f2e227467837a111
benny <shenbinsc@qq.com>
2021-09-02 16:10:03 +0800

    当日考勤查询打卡记录personid是字符串的处理

--- huaanClient/GetData.cs
+++ huaanClient/GetData.cs
@@ -4920,7 +4920,7 @@ namespace huaanClient
 
         public static dynamic[] getCaptureDataByIdForDate(string personId, DateTime date)
         {
-            var sql = $"SELECT ca.closeup, ca.time, ca.body_temp, dev.DeviceName, dev.number, dev.IsEnter from Capture_Data ca LEFT JOIN MyDevice dev ON ca.device_sn = dev.number WHERE person_id = {personId} and time LIKE '{date:yyyy-MM-dd}%' ORDER BY time";
+            var sql = $"SELECT ca.closeup, ca.time, ca.body_temp, dev.DeviceName, dev.number, dev.IsEnter from Capture_Data ca LEFT JOIN MyDevice dev ON ca.device_sn = dev.number WHERE person_id = '{personId}' and time LIKE '{date:yyyy-MM-dd}%' ORDER BY time";
             using (var conn = SQLiteHelper.GetConnection())
             {
                 return conn.Query(sql).ToArray();
