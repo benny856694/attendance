6eed60d733905caeae41842fe50495ae49035298
benny <shenbinsc@gmail.com>
2022-05-11 19:43:14 +0800

    基本计算正确

--- huaanClient/Report/Converter.cs
+++ huaanClient/Report/Converter.cs
@@ -31,8 +31,7 @@ namespace huaanClient.Report
             result.CheckOut2 = data.Punchinformation22.ToLocalTime();
             result.IsCrossMidnight = data.IsAcrossNight == "True";
             result.Temperature = data.temperature.toFloat();
-            CalcHours(result, result.ShiftStart1, result.ShiftEnd1, result.CheckIn1, result.CheckOut1);
-            CalcHours(result, result.ShiftStart2, result.ShiftEnd2, result.CheckIn2, result.CheckOut2);
+            CalcHours(result);
             result.Remark = result.CalcRemarks();
             if (result.Remark == Remark.Absence)
             {
@@ -45,36 +44,160 @@ namespace huaanClient.Report
             return result;
 
         }
-        
-        private static void CalcHours(DailyAttendanceData result, LocalTime? start, LocalTime? end, LocalTime? checkIn, LocalTime? checkOut)
+
+        private static void CalcOneCommuteHours()
         {
-            if (checkIn.HasValue && checkOut.HasValue)
+            
+        }
+
+        private static void CalcLateNEarlyHour(DailyAttendanceData result)
+        {
+            Period lateHours = Period.Zero;
+            Period earlyHours = Period.Zero;
+            if (result.CheckIn1.HasValue)
             {
-                if (checkIn.Value > start.Value)
+                if (result.CheckIn1.Value > result.ShiftStart1.Value)
                 {
-                    result.LateHour += (checkIn.Value - start.Value).Normalize();
+                    lateHours += result.CheckIn1.Value - result.ShiftStart1.Value;
+                }
+            }
+
+            if (result.CheckOut1.HasValue)
+            {
+                if (result.CheckOut1.Value < result.ShiftEnd1.Value)
+                {
+                    earlyHours += result.ShiftEnd1.Value - result.CheckOut1.Value;
+                }
+            }
+
+
+            if (result.CheckIn2.HasValue)
+            {
+                if (result.CheckIn2.Value > result.ShiftStart2.Value)
+                {
+                    lateHours += result.CheckIn2.Value - result.ShiftStart2.Value;
+                }
+            }
+
+
+            if (result.CheckOut2.HasValue)
+            {
+                if (result.CheckOut2.Value < result.ShiftEnd2.Value)
+                {
+                    earlyHours += result.ShiftEnd2.Value - result.CheckOut2.Value;
+                }
+            }
+
+            result.LateHour = lateHours.Normalize();
+            result.EarlyHour = earlyHours.Normalize();
+        }
+
+        private static void CalcWorkHourNOverTimeHour(DailyAttendanceData result, bool isFullPresent)
+        {
+            Period workHour = Period.Zero;
+            Period overTimeHours = Period.Zero;
+            LocalTime? checkInTime = null;
+
+            if (isFullPresent)
+            {
+                var breakHour = result.ShiftStart2.Value - result.ShiftEnd1.Value;
+                if (result.CheckOut1.HasValue && result.CheckIn2.HasValue)
+                {
+                    workHour += result.CheckOut1.Value - result.CheckIn1.Value;
+                    workHour += result.CheckOut2.Value - result.CheckIn2.Value;
+                }
+                else
+                {
+                    workHour += result.CheckOut2.Value - result.CheckIn1.Value;
+                    workHour -= breakHour;
+                }
+            }
+            else
+            {
+                if (result.CheckIn1.HasValue)
+                {
+                    checkInTime = result.CheckIn1;
                 }
 
-                if (checkOut.Value < end.Value)
+                if (result.CheckOut1.HasValue)
                 {
-                    result.EarlyHour += (end.Value - checkOut.Value).Normalize();
+                    if (checkInTime.HasValue)
+                    {
+                        var endTime = result.CheckOut1.Value;
+                        workHour += endTime - checkInTime.Value;
+                    }
+                    checkInTime = null;
+                }
+               
+
+                if (result.CheckIn2.HasValue)
+                {
+                    checkInTime = result.CheckIn2;
                 }
                 
-                var shiftHour = end.Value - start.Value;
-                var workHour = checkOut.Value - checkIn.Value;
-                if (result.IsCrossMidnight)
-                {
-                    shiftHour += Period.FromHours(24);
-                    workHour += Period.FromHours(24);
-                }
-                result.WorkHour += workHour.Normalize();
-                var ot = (workHour - shiftHour).Normalize();
-                if (!(ot.Hours < 0 || ot.Minutes < 0))
-                {
-                    result.OverTime += ot;
-                }
 
+                if (result.CheckOut2.HasValue)
+                {
+                    if (checkInTime.HasValue)
+                    {
+                        var endTime = result.CheckOut2.Value;
+                        workHour += endTime - checkInTime.Value;
+                    }
+                    checkInTime = null;
+                }
             }
+         
+
+            var shiftHour = Period.Zero;
+            if (result.ShiftStart1.HasValue && result.ShiftEnd1.HasValue)
+            {
+                shiftHour += result.ShiftEnd1.Value - result.ShiftStart1.Value;
+            }
+            if (result.ShiftStart2.HasValue && result.ShiftEnd2.HasValue)
+            {
+                shiftHour += result.ShiftEnd2.Value - result.ShiftStart2.Value;
+            }
+            var ot = (workHour - shiftHour).Normalize();
+            result.OverTime = ot.Normalize();
+            result.WorkHour = workHour.Normalize();
+        }
+        
+      
+        
+        private static void CalcHours(DailyAttendanceData result)
+        {
+            var isFullPresent = result.CheckIn1.HasValue && result.CheckOut2.HasValue;
+            CalcWorkHourNOverTimeHour(result, isFullPresent);
+            CalcLateNEarlyHour(result);
+
+
+            //if (checkIn.HasValue && checkOut.HasValue)
+            //{
+            //    if (checkIn.Value > start.Value)
+            //    {
+            //        result.LateHour += (checkIn.Value - start.Value).Normalize();
+            //    }
+
+            //    if (checkOut.Value < end.Value)
+            //    {
+            //        result.EarlyHour += (end.Value - checkOut.Value).Normalize();
+            //    }
+
+            //    var shiftHour = end.Value - start.Value;
+            //    var workHour = checkOut.Value - checkIn.Value;
+            //    if (result.IsCrossMidnight)
+            //    {
+            //        shiftHour += Period.FromHours(24);
+            //        workHour += Period.FromHours(24);
+            //    }
+            //    result.WorkHour += workHour.Normalize();
+            //    var ot = (workHour - shiftHour).Normalize();
+            //    if (!(ot.Hours < 0 || ot.Minutes < 0))
+            //    {
+            //        result.OverTime += ot;
+            //    }
+
+            //}
         }
 
 
