41f5179c1d107c9d4747347e05522442a859b7d6
BinShen <shenbinsc@gmail.com>
2022-10-31 18:25:46 +0800

    验证代码跑通

---
+++ huaanClient/BroadcastBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace huaanClient
+{
+    internal class BroadcastBehavior : WebSocketBehavior
+    {
+       
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Sessions.Broadcast(e.Data);
+        }
+
+        public static BroadcastBehavior Instance = new Lazy<BroadcastBehavior>(() => new BroadcastBehavior()).Value;
+        
+
+        public void Broadcast(string json)
+        {
+            Sessions?.Broadcast(json);
+        }
+    }
+}
