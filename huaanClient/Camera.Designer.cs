8c8d2ef3bf7c7e207ce3e25fc557008f745dc1b4
Benny Shen <shenbinsc@qq.com>
2022-11-28 20:07:32 +0800

    修复抓拍界面错乱

--- huaanClient/Camera.Designer.cs
+++ huaanClient/Camera.Designer.cs
@@ -31,21 +31,28 @@
             this.btnLogin = new ZXCL.WinFormUI.ZButton();
             this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
             this.plView = new System.Windows.Forms.FlowLayoutPanel();
+            this.skinSplitContainer1 = new CCWin.SkinControl.SkinSplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.skinSplitContainer1)).BeginInit();
+            this.skinSplitContainer1.Panel1.SuspendLayout();
+            this.skinSplitContainer1.Panel2.SuspendLayout();
+            this.skinSplitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnLogin
             // 
+            this.btnLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnLogin.BackColorHover = System.Drawing.Color.SkyBlue;
             this.btnLogin.BackColorMouseDown = System.Drawing.Color.DodgerBlue;
             this.btnLogin.BackColorNormal = System.Drawing.Color.DeepSkyBlue;
             this.btnLogin.BorderColorFocus = System.Drawing.Color.Transparent;
             this.btnLogin.BorderColorNormal = System.Drawing.Color.Transparent;
-            this.btnLogin.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.btnLogin.ForeColor = System.Drawing.Color.White;
-            this.btnLogin.Location = new System.Drawing.Point(212, 423);
+            this.btnLogin.Location = new System.Drawing.Point(283, 532);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Radius = 10;
-            this.btnLogin.Size = new System.Drawing.Size(350, 40);
+            this.btnLogin.Size = new System.Drawing.Size(467, 50);
             this.btnLogin.TabIndex = 26;
             this.btnLogin.Text = "拍   照";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -53,32 +60,58 @@
             // 
             // videoSourcePlayer1
             // 
-            this.videoSourcePlayer1.Location = new System.Drawing.Point(212, 49);
+            this.videoSourcePlayer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(0, 0);
+            this.videoSourcePlayer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.videoSourcePlayer1.Name = "videoSourcePlayer1";
-            this.videoSourcePlayer1.Size = new System.Drawing.Size(350, 350);
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(482, 457);
             this.videoSourcePlayer1.TabIndex = 27;
             this.videoSourcePlayer1.Text = "videoSourcePlayer1";
             this.videoSourcePlayer1.VideoSource = null;
             // 
             // plView
             // 
-            this.plView.Location = new System.Drawing.Point(19, 49);
-            this.plView.Name = "plView";
-            this.plView.Size = new System.Drawing.Size(187, 350);
-            this.plView.TabIndex = 28;
             this.plView.AutoScroll = true;
+            this.plView.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.plView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plView.Location = new System.Drawing.Point(0, 0);
+            this.plView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.plView.Name = "plView";
+            this.plView.Size = new System.Drawing.Size(243, 457);
+            this.plView.TabIndex = 28;
+            // 
+            // skinSplitContainer1
+            // 
+            this.skinSplitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.skinSplitContainer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.skinSplitContainer1.Location = new System.Drawing.Point(21, 55);
+            this.skinSplitContainer1.Name = "skinSplitContainer1";
+            // 
+            // skinSplitContainer1.Panel1
+            // 
+            this.skinSplitContainer1.Panel1.Controls.Add(this.plView);
+            // 
+            // skinSplitContainer1.Panel2
+            // 
+            this.skinSplitContainer1.Panel2.Controls.Add(this.videoSourcePlayer1);
+            this.skinSplitContainer1.Size = new System.Drawing.Size(729, 457);
+            this.skinSplitContainer1.SplitterDistance = 243;
+            this.skinSplitContainer1.TabIndex = 29;
             // 
             // Camera
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BorderColor = System.Drawing.Color.White;
             this.CaptionLeftSpacing = 10;
-            this.ClientSize = new System.Drawing.Size(577, 492);
-            this.Controls.Add(this.plView);
-            this.Controls.Add(this.videoSourcePlayer1);
+            this.ClientSize = new System.Drawing.Size(769, 615);
+            this.Controls.Add(this.skinSplitContainer1);
             this.Controls.Add(this.btnLogin);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Camera";
@@ -88,6 +121,10 @@
             this.Text = "拍照";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Camera_FormClosing);
             this.Load += new System.EventHandler(this.Camera_Load);
+            this.skinSplitContainer1.Panel1.ResumeLayout(false);
+            this.skinSplitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.skinSplitContainer1)).EndInit();
+            this.skinSplitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -96,5 +133,6 @@
         private ZXCL.WinFormUI.ZButton btnLogin;
         private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
         private System.Windows.Forms.FlowLayoutPanel plView;
+        private CCWin.SkinControl.SkinSplitContainer skinSplitContainer1;
     }
 }
