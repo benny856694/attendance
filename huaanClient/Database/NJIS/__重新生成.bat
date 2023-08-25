0b5d045e7b26a4908f5b43775aba84ab1f147c47
shen bin <shenbinsc@qq.com>
2023-08-25 15:09:01 +0800

    Revert "Merge branch 'NJIS' into dev"
    This reverts commit 985de76b018ae206a1ffc753d06754cbd0195418, reversing
    changes made to 8ab26f206461601c46816a511693cbd2b3ff2ca9.

--- huaanClient/Database/NJIS/__重新生成.bat
+++
@@ -1,2 +0,0 @@
-﻿
-FreeSql.Generator -Razor "__razor.cshtml.txt" -NameOptions 0,0,0,0 -NameSpace Attendance.Database.NJIS -DB "SqlServer,data source=.\sqlexpress;integrated security=True;initial catalog=NJIS;pooling=true;max pool size=2" -FileName "{name}.cs"
